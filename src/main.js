19a6868c6230ddb6b538e75897ab6d0f7ba14a1a
fpreciado3 <fpreciado3@ucol.mx>
2020-12-06 16:26:19 -0600

    implemented for loop for the function & the return of the variable

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ export default class App {
 
     sumarParesFor() {
         let suma = 0;
+
+        for(let b = 0; b<=20; b = b + 2){
+            suma = suma + b;
+        }
+
+        return suma;
     }
 }
 
